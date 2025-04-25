4de0f108e42dc31fd79776bcdddae57954b1f5b1
Shinaiii <lukas.stroede@gmail.com>
2025-04-26 00:15:26 +0200

    Changes to player and proj settings
    made player movable, changed gravity to 0 and light cropout to black in settings

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,24 @@
-extends Sprite2D
+extends CharacterBody2D
+
+var movementspeed = 100
+var grid_size = 16
+
+var selected_tile = Vector2()
 
 #scan tile at px position and interact with it
+func _physics_process(delta: float) -> void:
+	var motion = Vector2()
+	
+	motion.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	motion.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	
+	motion = motion * delta * movementspeed
+	move_and_collide(velocity)
+	
+	selected_tile = position - position % grid_size
+	
+	
+	
 
 #build: place selected tile
 
