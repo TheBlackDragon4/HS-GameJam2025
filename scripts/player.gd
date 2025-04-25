10470914f337977ec00e630061cf64e2f8231c24
Shinaiii <lukas.stroede@gmail.com>
2025-04-25 22:56:10 +0200

    Framework created
    Actions, scenes and tilemap initiated.

---
+++ scripts/player.gd
@@ -0,0 +1,9 @@
+extends Sprite2D
+
+#scan tile at px position and interact with it
+
+#build: place selected tile
+
+#destroy: destroy selected tile underneath if there is something placed
+
+#rotate: rotate a tiles sprite underneath
---
+++ scripts/world.gd
@@ -0,0 +1,3 @@
+extends Node2D
+
+#load all required scenes and spawn their instances on startup
