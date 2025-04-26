155a65f1a9581b5d3e957df80c9143142d10a691
Shinaiii <lukas.stroede@gmail.com>
2025-04-26 03:43:57 +0200

    merged and fixed movement and building
    you can move, build and destroy
    added animated scene for border

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,22 +1,29 @@
 extends CharacterBody2D
 
-var movementspeed = 100
-var grid_size = 16
-var controlled_by_mouse = true
-var sprite_position = Vector2()
+var _movementspeed = 100
+var _grid_size = 16
 
+var sprite_position = Vector2()
 var motion = Vector2()
 var selected_tile = Vector2i(0,0)
+var _map1 
+
+var tab = 0
+var index = 1
 
 #scan tile at px position and interact with it
 func _ready() -> void:
-	pass
-	
+	_map1 = get_parent().get_parent().find_child("map1")
+
 func _physics_process(delta: float) -> void:
+	handle_movement()
+	handle_inputs()
+	
+func handle_movement() -> void:
 	motion.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	motion.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	
-	velocity = motion.normalized() * movementspeed
+	velocity = motion.normalized() * _movementspeed
 	move_and_slide()
 	
 	if get_global_mouse_position() != Vector2(0, 0): # is there a mouse position?
@@ -24,71 +31,31 @@ func _physics_process(delta: float) -> void:
 	else:
 		sprite_position = position
 		
-	selected_tile.x = floor(sprite_position.x/grid_size)
-	selected_tile.y = floor(sprite_position.y/grid_size)
+	selected_tile.x = floor(sprite_position.x/_grid_size)
+	selected_tile.y = floor(sprite_position.y/_grid_size)
 	
-	sprite_position.x = selected_tile.x * grid_size + grid_size*.5
-	sprite_position.y = selected_tile.y * grid_size + grid_size*.5
+	sprite_position.x = selected_tile.x * _grid_size + _grid_size*.5
+	sprite_position.y = selected_tile.y * _grid_size + _grid_size*.5
 	
 	$"../Sprite2D".position = sprite_position
-	# selected_tile = Vector2(position.x, position.y)# position - position % grid_size
-	#
-#func _ready():	
-	#print(typeof(get_parent().get_node("map1")))
-	#get_parent().get_node("map1").set_cell(Vector2(-2, -2), 1, Vector2(-2, -2))
-
-var x = 0
-var y = 0
-
-func _process(delta):
-	if Input.is_action_just_pressed("up"):
-		print(get_parent().get_node("map1").get_cell_tile_data(Vector2(x, y)))
-		y -= 1
-		#get_parent().get_node("map1").set_cell(Vector2(x, y), 0, Vector2(x, y))
-		print(x, " ", y)
-		#get_parent().get_node("map1").set_cell(Vector2(x, y), 1, Vector2(x, y), 2)
-	if Input.is_action_just_pressed("right"):
-		print(get_parent().get_node("map1").get_cell_tile_data(Vector2(x, y)))
-		x += 1
-		#get_parent().get_node("map1").set_cell(Vector2(x, y), 0, Vector2(x, y))
-		print(x, " ", y)
-		#get_parent().get_node("map1").set_cell(Vector2(x, y), 1, Vector2(x, y), 2)
-	if Input.is_action_just_pressed("down"):
-		print(get_parent().get_node("map1").get_cell_tile_data(Vector2(x, y)))
-		y += 1
-		#get_parent().get_node("map1").set_cell(Vector2(x, y), 0, Vector2(x, y))
-		#ö
-		#get_parent().get_node("map1").set_cell(Vector2(x, y), 1, Vector2(x, y), 2)
-		print(x, " ", y)
-	if Input.is_action_just_pressed("left"):
-		print(get_parent().get_node("map1").get_cell_tile_data(Vector2(x, y)))
-		x -= 1
-		#get_parent().get_node("map1").set_cell(Vector2i(x, y), 1, Vector2i(0, 0), 2)
-		print(x, " ", y)
-	
-	#if Input.is_action_just_pressed("rotate"):
-		#print("Rotate")
-		##get_parent().get_node("map1").set_cell(Vector2(x, y), 0, Vector2(x, y), 1)
-		#get_parent().get_node("map1").set_cell(Vector2i(x, y), 0, Vector2i(0, 0), 1)
-	
+func handle_inputs() -> void:
+	if Input.is_action_just_pressed("destroy"):
+		print("destroy")
+		_map1.set_cell(selected_tile, 0, Vector2i(0, 0))
 	if Input.is_action_just_pressed("build"):
-		print("Rotate")
-		#get_parent().get_node("map1").set_cell(Vector2(x, y), 0, Vector2(x, y), 1)
-		get_parent().get_node("map1").set_cell(Vector2i(x, y), 0, Vector2i(0, 0), 2)
-		#get_parent().get_node("map1").set_cell(Vector2(x, y), 1, Vector2(0, 0), 1)
-		#get_parent().get_node("map1").place_scene
-		#print(get_parent().get_node("map1").get_cell_source_id(Vector2(x, y)))
-		
-		
-		# Move right.
-	#var build_tile = local_to_map()
-	#set_cel
-	#print(get_parent().get_node("map1").get_cell_source_id(Vector2(-2, -2)))
-	#get_cell_source_id
-	#print(selected_tile)
-	
-	$Area2D.get_overlapping_bodies()
-	
+		print("build")
+		_map1.set_cell(selected_tile, 0, Vector2i(0, 0), 2)
+	if Input.is_action_just_pressed("rotate"):
+		print("rotate")
+	if Input.is_action_just_pressed("tab left"):
+		print("rotate")
+	if Input.is_action_just_pressed("tab right"):
+		print("rotate")
+	if Input.is_action_just_pressed("menu left"):
+		print("rotate")
+	if Input.is_action_just_pressed("menu right"):
+		print("rotate")
+
 		
 #build: place selected tile
 
