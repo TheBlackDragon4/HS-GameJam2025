b1fb5ef5b01358abc1780dba0e6772ffb1944674
Shinaiii <83395994+Shinaiii@users.noreply.github.com>
2025-04-27 13:54:02 +0200

    mirrors are huuge

---
+++ scenes/machines/mirror2.gd
@@ -0,0 +1,4 @@
+extends RigidBody2D
+
+func absorb(dropped):
+	print("mirror says naynay")
--- scripts/machines/mirror.gd
+++ scripts/machines/mirror.gd
@@ -4,10 +4,8 @@ extends Node2D
 @export var sprite_down: Texture2D
 @export var sprite_left: Texture2D
 @export var sprite_up: Texture2D
-@export var collision_box: CollisionShape2D
+@export var node2D_rotate: Node2D
 
-@export var speed:float
-@export var spawn_distance:float
 @export var sprite:Sprite2D
 
 var rotation_state = 0
@@ -18,6 +16,7 @@ var shots = 0
 func rotate_90():
 	print("attempt to rotate ", name)
 	rotation_state = posmod(rotation_state+1, 4)
+	rotation_degrees = rotation_state * 90
 	match rotation_state:
 		0:
 			sprite.texture = sprite_right
@@ -27,4 +26,7 @@ func rotate_90():
 			sprite.texture = sprite_left
 		3:
 			sprite.texture = sprite_up
+			
+func absorb(dropped):
+	print("S")
 	
