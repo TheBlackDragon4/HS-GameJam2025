e3e8d359dc1cae8d9004779ce03810c51fe72c01
Schultheiß, Daniel <118031298+dany-cmd@users.noreply.github.com>
2025-04-27 04:28:51 +0200

    tilesets have cpuparticles2d
    If you place multiple collectors next to each other, they will connect
    todos are to differentiate between collectors (getting new light), relays and consumers (getting from multiple sources)

--- scenes/templates/particles_supplier.gd
+++ scenes/templates/particles_supplier.gd
@@ -1,6 +1,6 @@
 extends CPUParticles2D
 
-var target: Node2D
+@export var target: Node2D
 @export var source: Node2D
 @export var velocity: float
 @export var range: float
@@ -11,20 +11,22 @@ func _on_ready() -> void:
 		self.global_position = source.global_position
 	if target == null:
 		target = find_target()
-		print(target.global_position)
-	var vec = (target.global_position - self.global_position).normalized()
-	print(vec)
-	self.direction = vec
-	self.initial_velocity_max = velocity
-	self.initial_velocity_min = velocity
-	self.lifetime = self.global_position.distance_to(target.global_position) / velocity
+		#print(self.global_position, " ", target.global_position)
+	if target != null:
+		var vec = (target.global_position - self.global_position).normalized()
+		self.direction = vec
+		self.initial_velocity_max = velocity
+		self.initial_velocity_min = velocity
+		self.lifetime = self.global_position.distance_to(target.global_position) / velocity
 
 func find_target() -> Node2D:
 	var min_dist = INF
 	var closest: Node2D = null
-	for node in get_tree().get_nodes_in_group("Particles"):
+	for node : Sprite2D in get_tree().get_nodes_in_group("Particles"):
 		var dist = self.global_position.distance_to(node.global_position)
-		if dist < min_dist and dist <= range and source != node:
-			min_dist = dist
-			closest = node
+		if dist < min_dist and \
+			dist <= range and \
+			get_parent().get_instance_id() != node.get_instance_id():
+				min_dist = dist
+				closest = node
 	return closest
