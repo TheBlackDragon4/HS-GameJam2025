7ff9edd933d0e68af42094e761a7d71197173751
Shinaiii <lukas.stroede@gmail.com>
2025-04-27 10:37:27 +0200

    inserted new assets
    need to reassign and throw out deprecated ones
    -- duplicates on mass

--- scripts/collector.gd
+++ scripts/collector.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
-@export var sprite_up: Texture2D
-@export var sprite_left: Texture2D
 @export var sprite_right: Texture2D
 @export var sprite_down: Texture2D
-@export var sprite_up_inside: Texture2D
-@export var sprite_left_inside: Texture2D
-@export var sprite_right_inside: Texture2D
-@export var sprite_down_inside: Texture2D
+@export var sprite_left: Texture2D
+@export var sprite_up: Texture2D
+@export var inside_right: Texture2D
+@export var inside_down: Texture2D
+@export var inside_left: Texture2D
+@export var inside_up: Texture2D
 @export var speed:float
 @export var spawn_distance:float
 @export var sprite:Sprite2D
@@ -34,17 +34,17 @@ func rotate_90():
 	rotation_state = posmod(rotation_state+1, 4)
 	match rotation_state:
 		0:
-			sprite.texture = sprite_up
-			sprite_inside.texture = sprite_up_inside
-		1:
 			sprite.texture = sprite_right
-			sprite_inside.texture = sprite_right_inside
-		2:
+			sprite_inside.texture = inside_right
+		1:
 			sprite.texture = sprite_down
-			sprite_inside.texture = sprite_down_inside
-		3:
+			sprite_inside.texture = inside_down
+		2:
 			sprite.texture = sprite_left
-			sprite_inside.texture = sprite_left_inside
+			sprite_inside.texture = inside_left
+		3:
+			sprite.texture = sprite_up
+			sprite_inside.texture = inside_up
 			
 		
 
